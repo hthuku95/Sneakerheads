bef4f6ecda2ac8021030df4539708cc0057936ca
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-06-13 17:54:40 -0700

    updated the file fields

--- core/models.py
+++ core/models.py
@@ -76,16 +76,16 @@ class Item(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField( max_length=100)
     price = models.FloatField()
-    thumbnail = models.ImageField(blank=True,null=True)
+    thumbnail = models.FileField(blank=True,null=True)
     discount_price = models.FloatField(blank=True,null=True)
     label = models.CharField(choices=LABEL_CHOICES,max_length=1)
     category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
     slug = models.SlugField()
     description = models.TextField()
     date = models.DateTimeField( auto_now_add=True,blank=True,null=True)
-    display_image_one = models.ImageField( blank=True,null=True)
-    display_image_two = models.ImageField( blank=True,null=True)
-    display_image_three = models.ImageField( blank=True,null=True)
+    display_image_one = models.FileField( blank=True,null=True)
+    display_image_two = models.FileField( blank=True,null=True)
+    display_image_three = models.FileField( blank=True,null=True)
 
     def __str__(self):
         return self.title
